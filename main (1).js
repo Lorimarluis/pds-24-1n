a4abbcaeeb695e91045a5e937b941eb1e7727037
Lorimar Luis Castelan <162641424+Lorimarluis@users.noreply.github.com>
2024-03-26 19:14:58 -0300

    Add files via upload

---
+++ main (1).js
@@ -0,0 +1,12 @@
+/******************************************************************************
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+print('Hello World');
+var palavra1 = 'alfa';
+var palavra2 = 'beto'
+var resultado = palavra1+palavra2;
+print(resultado);
